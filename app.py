63acef77acacf8cc078d87e5418304259353f8ce
sameerab04 <sameerab04@gmail.com>
2024-05-27 11:29:06 -0500

    Pylint Updates

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import streamlit as st
+"""
+Recommender System Interface
+This Streamlit app allows users to generate recommendations using 
+collaborative filtering and content-based filtering models.
+"""
+
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 import pickle
 import pandas as pd
-from src.project_pipeline.aws_utils import load_from_s3
+import streamlit as st
+from dotenv import load_dotenv
+from  src.project_pipeline.aws_utils import load_from_s3
 import src.project_pipeline.load_config as lc
 
 # Load configuration and environment variables
@@ -18,46 +24,64 @@ aws_secret_access_key = os.getenv("aws_secret_access_key")
 aws_region = os.getenv("aws_region")
 bucket_name = config["aws"]["bucket_name"]
 
-
 @st.cache_resource
 def load_model(model_path):
+    """
+    Load a machine learning model from the specified path.
+
+    Parameters:
+    - model_path (str): The path to the model file.
+
+    Returns:
+    - model: The loaded machine learning model.
+    """
     with open(model_path, 'rb') as file:
         return pickle.load(file)
 
 def load_data(data_path):
+    """
+    Load data from the specified path.
+
+    Parameters:
+    - data_path (str): The path to the data file.
+
+    Returns:
+    - pd.DataFrame: The loaded data.
+    """
     return pd.read_pickle(data_path)
 
-def make_content_based_predictions(user_id, df_with_one_hot, pipeline, df_new):
-    # Get user name
-    user_name = df_new.loc[df_new['user_id'] == user_id, 'user_name'].iloc[0]
+def make_content_based_predictions(user_id, df_with_one_hot, pipeline):
+    """
+    Generate content-based recommendations for a given user.
 
-    # Get all unique product IDs from the original dataset
+    Parameters:
+    - user_id (str): The ID of the user for whom recommendations are to be generated.
+    - df_with_one_hot (pd.DataFrame): The dataframe containing one-hot encoded data.
+    - pipeline: The trained content-based filtering pipeline.
+
+    Returns:
+    - list: A list of recommendation dictionaries, 
+    each containing 'product_id' and 'predicted_rating'.
+    """
     all_product_ids = df_with_one_hot['product_id'].unique()
 
-    # Filter out products the user has already interacted with in the entire dataset
     user_interactions = df_with_one_hot[df_with_one_hot['user_id'] == user_id]['product_id'].values
-    unrated_product_ids = [product_id for product_id in all_product_ids if product_id not in user_interactions]
+    unrated_product_ids = [product_id for product_id in all_product_ids
+                            if product_id not in user_interactions]
 
-    # Create a dataset with the features of the unrated items
-    X_new = df_with_one_hot[df_with_one_hot['product_id'].isin(unrated_product_ids)]
-    X_new = X_new.drop(columns=['rating', 'user_id'])
+    x_new = df_with_one_hot[df_with_one_hot['product_id'].isin(unrated_product_ids)]
+    x_new = x_new.drop(columns=['rating', 'user_id'])
 
-    # Make predictions using the trained pipeline
-    predictions = pipeline.predict(X_new)
+    predictions = pipeline.predict(x_new)
 
-    # Combine predictions with item IDs
-    predicted_ratings = pd.DataFrame({'product_id': X_new['product_id'], 'predicted_rating': predictions})
-
-    # Sort predictions by predicted rating in descending order
+    predicted_ratings = pd.DataFrame({'product_id': x_new['product_id'],
+                                       'predicted_rating': predictions})
     predicted_ratings.sort_values(by='predicted_rating', ascending=False, inplace=True)
-
-    # Remove duplicates
     predicted_ratings = predicted_ratings.drop_duplicates(subset=['product_id'])
 
-    # Print top N recommendations
-    N = 10
+    num_recs = 10
     recommendations = []
-    for i, row in predicted_ratings.head(N).iterrows():
+    for _, row in predicted_ratings.head(num_recs).iterrows():
         recommendation = {
             "product_id": row['product_id'],
             "predicted_rating": row['predicted_rating']
@@ -66,26 +90,105 @@ def make_content_based_predictions(user_id, df_with_one_hot, pipeline, df_new):
 
     return recommendations
 
+def generate_cf_recommendations(model, user_id, df_with_one_hot):
+    """
+    Generates collaborative filtering recommendations based on the selected model and user input.
+
+    Parameters:
+    - model: The collaborative filtering model.
+    - user_id (str): The ID of the user for whom recommendations are to be generated.
+    - df_with_one_hot (pd.DataFrame): The dataframe containing one-hot encoded data.
+
+    Returns:
+    None
+    """
+    predictions = []
+    for product_id in df_with_one_hot['product_id'].unique():
+        pred = model.predict(uid=user_id, iid=str(product_id))
+        predictions.append((product_id, pred.est))
+
+
+
+    predicted_ratings = pd.DataFrame(predictions, columns=['product_id', 'predicted_rating'])
+    predicted_ratings.sort_values(by='predicted_rating', ascending=False, inplace=True)
+    predicted_ratings = predicted_ratings.drop_duplicates(subset=['product_id'])
+
+    num_recs = 10
+    top_recommendations = predicted_ratings.head(num_recs)
+    st.write(f"Top {num_recs} recommendations for user {user_id}:")
+    st.dataframe(top_recommendations)
+
+
+def generate_cbf_recommendations(user_id, df_with_one_hot):
+    """
+    Generates content-based filtering recommendations based on the selected model and user input.
+
+    Parameters:
+    - user_id (str): The ID of the user for whom recommendations are to be generated.
+    - df_with_one_hot (pd.DataFrame): The dataframe containing one-hot encoded data.
+
+    Returns:
+    None
+    """
+    content_based_model_path = Path('artifacts/Content_Based_Filtering/best_cbf.pkl')
+    if not content_based_model_path.exists():
+        st.error(f"Model file not found: {content_based_model_path}")
+        return
+
+    content_based_pipeline = load_model(content_based_model_path)
+    st.write("Content Based Filtering model loaded successfully!")
+    recommendations = make_content_based_predictions(user_id, df_with_one_hot,
+                                                     content_based_pipeline)
+    num_recs = 10
+    st.write(f"Top {num_recs} recommendations for user {user_id}:")
+    st.write(pd.DataFrame(recommendations))
+
+
+def generate_recommendations(model_choice, user_id, model, df_with_one_hot):
+    """
+    Generates recommendations based on the selected model and user input.
+
+    Parameters:
+    - model_choice (str): The choice of model for generating recommendations.
+    - user_id (str): The ID of the user for whom recommendations are to be generated.
+    - model: The collaborative filtering model object.
+    - df_with_one_hot (pd.DataFrame): The dataframe containing one-hot encoded data.
+
+    Returns:
+    None
+    """
+    if model_choice == 'Collaborative Filtering':
+        generate_cf_recommendations(model, user_id, df_with_one_hot)
+    elif model_choice == 'Content Based Filtering':
+        generate_cbf_recommendations(user_id, df_with_one_hot)
+
+
 
 def main():
+    """
+    Main function to run the Streamlit app.
+    """
     st.title('Recommender System Interface')
 
-    # Load and cache data
     data_path = Path("artifacts/Data/final_df.pkl")
     if data_path.exists():
         df_with_one_hot = load_data(data_path)
-        all_product_ids = df_with_one_hot['product_id'].unique()
     else:
         st.error("Data file not found. Please check your setup.")
+        return
 
     if st.button('Download Artifacts from S3'):
-        target_directories = ['artifacts_Collaborative_Filtering', 'artifacts_Content_Based_Filtering', 'artifacts_Data']
-        load_from_s3(aws_access_key, aws_secret_access_key, aws_region, bucket_name, target_directories)
+        target_directories = ['artifacts_Collaborative_Filtering',
+                        'artifacts_Content_Based_Filtering',
+                              'artifacts_Data']
+        load_from_s3(aws_access_key, aws_secret_access_key,
+                     aws_region, bucket_name, target_directories)
         st.session_state['models_downloaded'] = True
-        st.success("Artifacts downloaded successfully!")
 
+    # Check if models are downloaded before proceeding
     if st.session_state.get('models_downloaded', False):
-        model_choice = st.selectbox('Select Model', ['Collaborative Filtering', 'Content Based Filtering'])
+        model_choice = st.selectbox('Select Model',
+                                    ['Collaborative Filtering', 'Content Based Filtering'])
         model_paths = {
             'Collaborative Filtering': Path('artifacts/Collaborative_Filtering/best_cf.pkl'),
             'Content Based Filtering': Path('artifacts/Content_Based_Filtering/best_cbf.pkl')
@@ -95,39 +198,16 @@ def main():
             st.write(f"{model_choice} model loaded successfully!")
         else:
             st.error(f"Model file not found: {model_paths[model_choice]}")
+            return
 
         user_id = st.text_input('Enter User ID:')
         if st.button('Generate Recommendations'):
             if user_id:
-                if model_choice == 'Collaborative Filtering':
-                    predictions = []
-                    for product_id in all_product_ids:
-                        try:
-                            pred = model.predict(uid=user_id, iid=str(product_id))
-                            predictions.append((product_id, pred.est))
-                        except Exception as e:
-                            st.error(f"Failed to make prediction for product {product_id}: {e}")
-
-                    predicted_ratings = pd.DataFrame(predictions, columns=['product_id', 'predicted_rating'])
-                    predicted_ratings.sort_values(by='predicted_rating', ascending=False, inplace=True)
-                    predicted_ratings = predicted_ratings.drop_duplicates(subset=['product_id'])
-                    N = 10
-                    top_recommendations = predicted_ratings.head(N)
-                    st.write(f"Top {N} recommendations for user {user_id}:")
-                    st.dataframe(top_recommendations)
-                elif model_choice == 'Content Based Filtering':
-                    # Load Content-Based Filtering model
-                    content_based_model_path = Path('artifacts/Content_Based_Filtering/best_cbf.pkl')
-                    if content_based_model_path.exists():
-                        content_based_pipeline = load_model(content_based_model_path)
-                        st.write("Content Based Filtering model loaded successfully!")
-                        recommendations = make_content_based_predictions(user_id, df_with_one_hot, content_based_pipeline, df_with_one_hot)
-                        st.write(f"Top 10 recommendations for user {user_id}:")
-                        st.write(pd.DataFrame(recommendations))
-                    else:
-                        st.error(f"Model file not found: {content_based_model_path}")
+                generate_recommendations(model_choice, user_id, model, df_with_one_hot)
             else:
                 st.error("Please enter a valid User ID.")
+
+
 
 if __name__ == "__main__":
     main()
--- pipeline.py
+++ pipeline.py
@@ -1,24 +1,24 @@
+"""
+Pipeline Module
+This module defines a pipeline for loading data, preprocessing it, training models,
+and saving artifacts.
+"""
+
 import os
 import logging
+from pathlib import Path
 from dotenv import load_dotenv
 import pandas as pd
-from pathlib import Path
-import src.project_pipeline.load_config as lc
-import src.project_pipeline.data_loader as dl
-import src.project_pipeline.eda as eda
-import src.project_pipeline.model_training as mt
-import src.project_pipeline.save_artifacts as sa
-import src.project_pipeline.aws_utils as aws
+from src.project_pipeline import eda, load_config
+from src.project_pipeline import data_loader, model_training, save_artifacts, aws_utils
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-CONFIG_PATH = os.getenv("CONFIG_PATH", "config/default.yaml")
-config = lc.load_config(Path(CONFIG_PATH))
-
 # Load configuration and environment variables
+CONFIG_PATH = os.getenv("CONFIG_PATH", "config/default.yaml")
+config = load_config.load_config(Path(CONFIG_PATH))
 load_dotenv()
 aws_access_key = os.getenv("aws_access_key_id")
 aws_secret_access_key = os.getenv("aws_secret_access_key")
@@ -34,37 +34,48 @@ TRAIN_DATA_PATH = artifacts / "Data" / "train_data.pkl"
 TEST_DATA_PATH = artifacts / "Data" / "test_data.pkl"
 
 logger.info("Reading data...")
-df = dl.read_data(config['data_loader']['path'])
+df = data_loader.read_data(config['data_loader']['path'])
 
 logger.info("Preprocessing data...")
 df_processed = eda.data_preprocess(df)
-df_user_split = pd.concat([eda.split_users(row) for _, row in df_processed.iterrows()], ignore_index=True)
+df_user_split = pd.concat([eda.split_users(row)
+                           for _, row in df_processed.iterrows()], ignore_index=True)
 
 logger.info("Extracting first and last category...")
-df_user_split[['First_category', 'Last_category']] = df_user_split['category'].apply(lambda x: pd.Series(eda.extract_first_last(x)))
+df_user_split[['First_category', 'Last_category']] = df_user_split['category'].apply(lambda x:
+                                                        pd.Series(eda.extract_first_last(x)))
 df_user_split.drop('category', axis=1, inplace=True)
 
 logger.info("Performing one-hot encoding...")
 df_final = eda.one_hot_encoding(df_user_split)
 
 logger.info("Splitting data into train and test sets...")
-train_test_data = mt.train_test_data(df_final, config["train_test_config"]["test_size"], config["train_test_config"]["random_state"], config["train_test_config"]["training_cols"])
+train_test_data = model_training.train_test_data(df_final, config["train_test_config"]["test_size"],
+                                     config["train_test_config"]["random_state"],
+                                     config["train_test_config"]["training_cols"])
 
 logger.info("Training Collaborative Filtering model...")
-best_collaborative_filtering = mt.collaborative_filtering(train_test_data[0], **config["model_building"][0]['CF'][1]["params"])
+best_collaborative_filtering = model_training.collaborative_filtering(train_test_data[0],
+                                    **config["model_building"][0]['CF'][1]["params"])
 
 logger.info("Training Content Based Filtering model...")
-content_based_filtering = mt.content_base_filtering(config["model_building"][1]["CBF"][0]["numeric_params"], config["model_building"][1]["CBF"][0]["text_params"], train_test_data[1])
+content_based_filtering = model_training.content_base_filtering(
+                            config["model_building"][1]["CBF"][0]["numeric_params"],
+                            config["model_building"][1]["CBF"][0]["text_params"],
+                            train_test_data[1])
 
 logger.info("Saving models and data...")
-sa.save_model(content_based_filtering, CBF_MODEL_FILE)
-sa.save_model(best_collaborative_filtering, CF_MODEL_FILE)
-sa.save_data(df_user_split, DATA_USER_SPLIT)
-sa.save_data(df_final, DATA_BEFORE_TRAIN_PATH)
-sa.save_data(train_test_data[1], TRAIN_DATA_PATH)
-sa.save_data(train_test_data[2], TEST_DATA_PATH)
+save_artifacts.save_model(content_based_filtering, CBF_MODEL_FILE)
+save_artifacts.save_model(best_collaborative_filtering, CF_MODEL_FILE)
+save_artifacts.save_data(df_user_split, DATA_USER_SPLIT)
+save_artifacts.save_data(df_final, DATA_BEFORE_TRAIN_PATH)
+save_artifacts.save_data(train_test_data[1], TRAIN_DATA_PATH)
+save_artifacts.save_data(train_test_data[2], TEST_DATA_PATH)
 
 logger.info("Uploading artifacts to AWS S3...")
-aws.upload_artifacts(aws_access_key, aws_secret_access_key, aws_region, artifacts, config['aws'])
+aws_utils.upload_artifacts(aws_access_key,
+                           aws_secret_access_key,
+                           aws_region, artifacts,
+                           config['aws'])
 
 logger.info("Uploaded artifacts!")
