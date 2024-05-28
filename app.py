bc73a11000ec3bea143d87fecc4a00732eb1c8d3
sameerab04 <sameerab04@gmail.com>
2024-05-27 20:06:24 -0500

    Update Readme

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def load_model(model_path):
     Returns:
     - model: The loaded machine learning model.
     """
-    with open(model_path, 'rb') as file:
+    with open(model_path, "rb") as file:
         return pickle.load(file)
 
 def load_data(data_path):
@@ -61,30 +61,30 @@ def make_content_based_predictions(user_id, df_with_one_hot, pipeline):
 
     Returns:
     - list: A list of recommendation dictionaries, 
-    each containing 'product_id' and 'predicted_rating'.
+    each containing "product_id" and "predicted_rating".
     """
-    all_product_ids = df_with_one_hot['product_id'].unique()
+    all_product_ids = df_with_one_hot["product_id"].unique()
 
-    user_interactions = df_with_one_hot[df_with_one_hot['user_id'] == user_id]['product_id'].values
+    user_interactions = df_with_one_hot[df_with_one_hot["user_id"] == user_id]["product_id"].values
     unrated_product_ids = [product_id for product_id in all_product_ids
                             if product_id not in user_interactions]
 
-    x_new = df_with_one_hot[df_with_one_hot['product_id'].isin(unrated_product_ids)]
-    x_new = x_new.drop(columns=['rating', 'user_id'])
+    x_new = df_with_one_hot[df_with_one_hot["product_id"].isin(unrated_product_ids)]
+    x_new = x_new.drop(columns=["rating", "user_id"])
 
     predictions = pipeline.predict(x_new)
 
-    predicted_ratings = pd.DataFrame({'product_id': x_new['product_id'],
-                                       'predicted_rating': predictions})
-    predicted_ratings.sort_values(by='predicted_rating', ascending=False, inplace=True)
-    predicted_ratings = predicted_ratings.drop_duplicates(subset=['product_id'])
+    predicted_ratings = pd.DataFrame({"product_id": x_new["product_id"],
+                                       "predicted_rating": predictions})
+    predicted_ratings.sort_values(by="predicted_rating", ascending=False, inplace=True)
+    predicted_ratings = predicted_ratings.drop_duplicates(subset=["product_id"])
 
     num_recs = 10
     recommendations = []
     for _, row in predicted_ratings.head(num_recs).iterrows():
         recommendation = {
-            "product_id": row['product_id'],
-            "predicted_rating": row['predicted_rating']
+            "product_id": row["product_id"],
+            "predicted_rating": row["predicted_rating"]
         }
         recommendations.append(recommendation)
 
@@ -103,15 +103,15 @@ def generate_cf_recommendations(model, user_id, df_with_one_hot):
     None
     """
     predictions = []
-    for product_id in df_with_one_hot['product_id'].unique():
+    for product_id in df_with_one_hot["product_id"].unique():
         pred = model.predict(uid=user_id, iid=str(product_id))
         predictions.append((product_id, pred.est))
 
 
 
-    predicted_ratings = pd.DataFrame(predictions, columns=['product_id', 'predicted_rating'])
-    predicted_ratings.sort_values(by='predicted_rating', ascending=False, inplace=True)
-    predicted_ratings = predicted_ratings.drop_duplicates(subset=['product_id'])
+    predicted_ratings = pd.DataFrame(predictions, columns=["product_id", "predicted_rating"])
+    predicted_ratings.sort_values(by="predicted_rating", ascending=False, inplace=True)
+    predicted_ratings = predicted_ratings.drop_duplicates(subset=["product_id"])
 
     num_recs = 10
     top_recommendations = predicted_ratings.head(num_recs)
@@ -130,7 +130,7 @@ def generate_cbf_recommendations(user_id, df_with_one_hot):
     Returns:
     None
     """
-    content_based_model_path = Path('artifacts/Content_Based_Filtering/best_cbf.pkl')
+    content_based_model_path = Path("artifacts/Content_Based_Filtering/best_cbf.pkl")
     if not content_based_model_path.exists():
         st.error(f"Model file not found: {content_based_model_path}")
         return
@@ -157,9 +157,9 @@ def generate_recommendations(model_choice, user_id, model, df_with_one_hot):
     Returns:
     None
     """
-    if model_choice == 'Collaborative Filtering':
+    if model_choice == "Collaborative Filtering":
         generate_cf_recommendations(model, user_id, df_with_one_hot)
-    elif model_choice == 'Content Based Filtering':
+    elif model_choice == "Content Based Filtering":
         generate_cbf_recommendations(user_id, df_with_one_hot)
 
 
@@ -168,7 +168,7 @@ def main():
     """
     Main function to run the Streamlit app.
     """
-    st.title('Recommender System Interface')
+    st.title("Recommender System Interface")
 
     data_path = Path("artifacts/Data/final_df.pkl")
     if data_path.exists():
@@ -177,21 +177,21 @@ def main():
         st.error("Data file not found. Please check your setup.")
         return
 
-    if st.button('Download Artifacts from S3'):
-        target_directories = ['artifacts_Collaborative_Filtering',
-                        'artifacts_Content_Based_Filtering',
-                              'artifacts_Data']
+    if st.button("Download Artifacts from S3"):
+        target_directories = ["artifacts_Collaborative_Filtering",
+                        "artifacts_Content_Based_Filtering",
+                              "artifacts_Data"]
         load_from_s3(aws_access_key, aws_secret_access_key,
                      aws_region, bucket_name, target_directories)
-        st.session_state['models_downloaded'] = True
+        st.session_state["models_downloaded"] = True
 
     # Check if models are downloaded before proceeding
-    if st.session_state.get('models_downloaded', False):
-        model_choice = st.selectbox('Select Model',
-                                    ['Collaborative Filtering', 'Content Based Filtering'])
+    if st.session_state.get("models_downloaded", False):
+        model_choice = st.selectbox("Select Model",
+                                    ["Collaborative Filtering", "Content Based Filtering"])
         model_paths = {
-            'Collaborative Filtering': Path('artifacts/Collaborative_Filtering/best_cf.pkl'),
-            'Content Based Filtering': Path('artifacts/Content_Based_Filtering/best_cbf.pkl')
+            "Collaborative Filtering": Path("artifacts/Collaborative_Filtering/best_cf.pkl"),
+            "Content Based Filtering": Path("artifacts/Content_Based_Filtering/best_cbf.pkl")
         }
         if model_paths[model_choice].exists():
             model = load_model(model_paths[model_choice])
@@ -200,8 +200,8 @@ def main():
             st.error(f"Model file not found: {model_paths[model_choice]}")
             return
 
-        user_id = st.text_input('Enter User ID:')
-        if st.button('Generate Recommendations'):
+        user_id = st.text_input("Enter User ID:")
+        if st.button("Generate Recommendations"):
             if user_id:
                 generate_recommendations(model_choice, user_id, model, df_with_one_hot)
             else:
